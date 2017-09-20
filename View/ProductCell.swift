ffbe6fb7e11a1f08f97e87d108face5a436ee4f5
Mac <mac@pc11.home>
2017-09-20 18:52:45 +0200

    second UI

---
+++ View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Mac on 9/20/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
