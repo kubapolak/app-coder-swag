e479042e2a06f9d3f1d092abfbbec9a73c3ec30f
Mac <mac@pc11.home>
2017-09-20 19:38:57 +0200

    populating collection view + last touches

--- Controller/CategoriesVC.swift
+++ Controller/CategoriesVC.swift
@@ -35,7 +35,23 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         }
     }
     
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            
+            let barBttn = UIBarButtonItem()
+            barBttn.title = ""
+            navigationItem.backBarButtonItem = barBttn
+            
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        }
+    }
 
 }
 
